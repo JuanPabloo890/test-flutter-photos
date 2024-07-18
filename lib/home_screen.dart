1b660d954d1015aacca6659ce600d787325c00d8
Juan Diego <102696740+juandiego252@users.noreply.github.com>
2024-07-17 19:55:14 -0500

    Update home_screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/services.dart' show PlatformException;
+import 'package:permission_handler/permission_handler.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -10,10 +11,21 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  Uint8List? _imageBytes; // Utiliza Uint8List para manejar imágenes en Flutter web
+  Uint8List?
+      _imageBytes; // Utiliza Uint8List para manejar imágenes en Flutter web
   final picker = ImagePicker();
 
   Future<void> getImage() async {
+        var status = await Permission.camera.status;
+    if (!status.isGranted) {
+      status = await Permission.camera.request();
+      if (!status.isGranted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Camera permission denied')),
+        );
+        return;
+      }
+    }
     try {
       final pickedFile = await picker.pickImage(source: ImageSource.camera);
 
@@ -27,43 +39,52 @@ class _HomeScreenState extends State<HomeScreen> {
           });
         } else {
           print('No image selected.');
+          setState(() {
+            _imageBytes = null;
+          });
         }
       });
     } on PlatformException catch (e) {
       print('Error selecting image: $e');
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error selecting image. Please try again later.')),
+        SnackBar(
+            content: Text('Error selecting image. Please try again later.')),
       );
     } catch (e) {
       print('Error selecting image: $e');
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error selecting image. Please try again later.')),
+        SnackBar(
+            content: Text('Error selecting image. Please try again later.')),
       );
     }
   }
 
   Future<void> uploadImage() async {
     if (_imageBytes == null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('No image selected.'))
-      );
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('No image selected.')));
       return;
     }
 
     try {
-      final storageRef = FirebaseStorage.instance.ref().child('images/${DateTime.now().toIso8601String()}');
-      final uploadTask = storageRef.putData(_imageBytes!); // Sube los bytes de la imagen
+      final storageRef = FirebaseStorage.instance
+          .ref()
+          .child('images/${DateTime.now().toIso8601String()}');
+      final uploadTask =
+          storageRef.putData(_imageBytes!); // Sube los bytes de la imagen
 
-      await uploadTask.whenComplete(() => {}); // Espera a que se complete la carga
-      final downloadUrl = await storageRef.getDownloadURL(); // Obtiene la URL de descarga
+      await uploadTask
+          .whenComplete(() => {}); // Espera a que se complete la carga
+      final downloadUrl =
+          await storageRef.getDownloadURL(); // Obtiene la URL de descarga
       print('Uploaded image URL: $downloadUrl');
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Image uploaded successfully.'))
-      );
+          SnackBar(content: Text('Image uploaded successfully.')));
     } catch (e) {
       print('Failed to upload image: $e');
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Failed to upload image. Please try again later.')),
+        SnackBar(
+            content: Text('Failed to upload image. Please try again later.')),
       );
     }
   }
@@ -79,13 +100,23 @@ class _HomeScreenState extends State<HomeScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             _imageBytes == null
-              ? Text('No image selected.')
-              : Image.memory(
-                  _imageBytes!,
-                  width: 300,
-                  height: 300,
-                  fit: BoxFit.cover,
-                ),
+                ? Column(
+                    children: [
+                      Image.asset(
+                        'assets/image/uploadImage.jpg',
+                        width: 300,
+                        height: 300,
+                        fit: BoxFit.cover,
+                      ),
+                      Text('Selecciona una imagen')
+                    ],
+                  )
+                : Image.memory(
+                    _imageBytes!,
+                    width: 300,
+                    height: 300,
+                    fit: BoxFit.cover,
+                  ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: getImage,
